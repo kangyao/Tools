59b89926f890942e5cce2c799549387b379fba0d
姚康 <oyakgna@gmail.com>
2025-05-27 21:29:16 +0800

    backup

---
+++ backup/pull.bat
@@ -0,0 +1,2 @@
+copy D:\GitHub\UnityDemo\Backup\bin\Release\*.* D:\GitHub\Command\WinCommand\backup\*.* /y
+copy D:\GitHub\UnityDemo\Backup\config.json D:\GitHub\Command\WinCommand\backup\config.json /y
